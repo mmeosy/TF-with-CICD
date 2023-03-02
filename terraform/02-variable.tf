131d8cce0d86f0927feaf394e148268228b23763
mmeosy <mehmetfidan94@hotmail.de>
2023-03-02 13:31:05 +0100

    replaced the code

--- terraform/02-variable.tf
+++ terraform/02-variable.tf
@@ -5,19 +5,3 @@ variable "rg_prefix" {
 variable "location" {
   default = "eastus"
 }
-
-variable "subscription_id_value" {
-  default = "e2482b33-30d4-4953-b603-01ace025e1fd"
-}
-
-variable "client_id_value" {
-  default = "4aca4770-0036-4991-9bda-f4bd5ba549b5"
-}
-
-variable "client_secret_value" {
-  default = "Cpc8Q~S.Wc5jMgaDfCbKx1bBwk9kWi46ESla1bHQ"
-}
-
-variable "tenant_id_value" {
-  default = "15cb2855-06a5-4006-ad14-d2a534fc72c6"
-}
