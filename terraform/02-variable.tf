9397abf80f3f436219565d9e1f3c3cc5ebe06642
mmeosy <mehmetfidan94@hotmail.de>
2023-03-02 12:59:02 +0100

    added git ignore and changed the secrets pass

--- terraform/02-variable.tf
+++ terraform/02-variable.tf
@@ -5,3 +5,19 @@ variable "rg_prefix" {
 variable "location" {
   default = "eastus"
 }
+
+variable "subscription_id_value" {
+  default = "e2482b33-30d4-4953-b603-01ace025e1fd"
+}
+
+variable "client_id_value" {
+  default = "8e572887-e2a3-44ee-bb52-1f2917b71367"
+}
+
+variable "client_secret_value" {
+  default = "hcW8Q~Sx5FDDCoYtR-Q4PDeD25ojHaGIpC4Ygagx"
+}
+
+variable "tenant_id_value" {
+  default = "15cb2855-06a5-4006-ad14-d2a534fc72c6"
+}
